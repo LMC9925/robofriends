731d144a7fc222f548c55ed4011b6082e7956b9b
LMC9925 <mingchakli1990@gmail.com>
2023-07-27 23:05:34 +0800

    Can search and load robots from API

--- src/CardList.js
+++ src/CardList.js
@@ -2,13 +2,13 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ( {robots} ) => {
-    const cardComponent = robots.map((user, i) => {
+    const cardComponent = robots.map((user) => {
         return (
             <Card 
-                key={robots[i].id} 
-                id={robots[i].id} 
-                name={robots[i].name} 
-                email={robots[i].email} />
+                key={user.id} 
+                id={user.id} 
+                name={user.name} 
+                email={user.email} />
         );
     });
     return(
